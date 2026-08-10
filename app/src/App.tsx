2e2663259ee3bd1c5ae2f4ffcb75819d755c115c
Nirav Dongre <nsd3284@gmail.com>
2026-08-10 18:59:40 +0530

    Added a drastic change in file

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,26 +3,56 @@ import Canvas from "./Component/Canvas"
 
 
 function App(){
+  const clientId = "124343";
+  const [ Messages , setMessages] = useState([]);
+  const [ Input, setInput ] = useState("");
+  const [ Socket, setSocket ] = useState<WebSocket | null>(null);
 
-  const [ Messages , setMessages] = useState("");
-
+  if(!Socket == null){
+    return <div className="h-screen flex justify-center items-center">
+      ...Loading
+    </div>
+  }
 
   useEffect(() => {
+
     const something = new WebSocket("ws://localhost:8080");
     something.onopen = () => {
-      console.log("It's on the websocket is!!!!")
+      const payload = {
+        "type": "join-room",
+        "roomId": "1234",
+        "clientId": clientId
+      }
+
+          something.send(JSON.stringify(payload));
+      console.log("Connecting on now you can try sending messages")
     }
 
     something.onmessage = (message) => {
-      const response = JSON.stringify(message.data)
-      console.log(response)
+      const response = JSON.parse(message.data);
+
+      if(response.type === "connect"){
+        console.log(`Connection established the id is ${response.clientId}`)
+      }
+
+      if(response.type === "chat-room"){
+        setMessages(prev => [...prev, response.messages])
+        console.log(`The answer you get in input is ${response.messages}`)
+      }
     }
+    setSocket(something);
+
+    return () => {
+    something.onclose = () => {
+      console.log("Connection closed")
+    }}
+
   }, [])
 
   return (
     <div>
       <div className="text-center">
-        Guess the word : g_h_r
+        Guess the word : {GuessWord}
       </div>
           <div className="h-screen flex justify-between items-center">
             <div>
@@ -38,13 +68,41 @@ function App(){
               </div>
             </div>
           <Canvas />
-          <div className="h-full p-10 flex justify-center items-center">
-                <div className="bg-blue-400 rounded-xl h-[800px] w-[400px]">
-                  <div className="flex flex-col mt-auto text-white p-2">
-                  <input type="text" placeholder="Chat" className="p-4 bg-blue-800 outline-none text-slate-300 rounded-xl" />
-                  </div>
-                </div>
-          </div>
+
+      <div className="h-full p-10 flex justify-center items-center">
+            <div className="bg-blue-400 rounded-xl h-[800px] w-[400px]">
+              <div className="flex flex-col mt-auto text-white p-2">
+                <div className="flex justify-between">
+                    <input onChange={(e) => {
+                      setInput(e.target.value)
+                    }} type="text" placeholder="Chat" className="p-4 bg-blue-800 outline-none text-slate-300 rounded-xl" />
+                              <button onClick={() => {
+                                console.log("button clicked")
+                                const payload = {
+                                  "type": "chat-room",
+                                  "roomId": "1234",
+                                  "clientId": clientId,
+                                  "messages": Input
+                                }
+                                Socket.send(JSON.stringify(payload))
+                   }} className="bg-white rounded-xl text-black p-4">Send</button>
+               </div>
+              </div>
+
+              <div className="p-2 flex flex-col gap-4">
+                {Messages.map(allmessages => {
+                  const keydrop = Math.random()*100000
+                  return(
+                    <div key={keydrop} className="p-3 rounded-xl bg-blue-300">
+                      {allmessages}
+                    </div>
+                  )
+                })}
+              </div>
+
+            </div>
+      </div>
+
           </div>
     </div>
   )
--- ws/src/index.ts
+++ ws/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json } from "express";
 import WebSocket ,{ WebSocketServer } from "ws";
 import cors from "cors";
 
@@ -13,11 +13,23 @@ interface Rooms {
     sockets: WebSocket[] 
 }
 
+const client : Record<string, { Connection: WebSocket }>= {}
+const ConnectorId = guid();
+
+function guid() {
+    var S4 = function() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
+
 const rooms : Record<string, Rooms> = {}
 
 const wss = new WebSocketServer({ server: httpServer});
 
 wss.on("connection", (ws) => {
+
+    console.log(`Connection established with browser it's name or id is this ${ConnectorId}`)
     
     ws.on("message", (data, isBinary) => {
 
@@ -31,24 +43,32 @@ wss.on("connection", (ws) => {
                 sockets: []
              }
             }
-            console.log(`User ${parsedData.userId} joined`)
+            console.log(`User ${parsedData.clientId} joined`)
         rooms[roomId].sockets.push(ws)
-    }
+        }
 
         if(parsedData.type === "chat-room"){
             rooms[roomId]?.sockets.forEach(socket =>{
-                if(socket == ws) return;
-                socket.send(message)
+                socket.send(JSON.stringify(parsedData))
             })
         console.log(parsedData.messages)
     }
 
         if(parsedData.type === "leave-room"){
             if(rooms[roomId]?.sockets){
-                rooms[roomId].sockets = rooms[roomId]?.sockets.filter((socket) => {
-                socket !== ws
-            })
+            rooms[roomId].sockets = rooms[roomId]?.sockets.filter((socket) => { socket !== ws })
         }
     }
     })
+
+    client[ConnectorId] = {
+        "Connection": ws
+    };
+
+    const payload = {
+        "type": "connect",
+        "clientId": ConnectorId
+    }
+
+    ws.send(JSON.stringify(payload))
 })
